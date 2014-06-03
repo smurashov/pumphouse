3cc1350ef969af3782f33d82de4d011d2176a42d
Oleg Gelbukh <ogelbukh@mirantis.com>
2014-06-03 15:26:41 +0000

    Add Tenant resource class

---
+++ pumphouse/resources/tenant.py
@@ -0,0 +1,25 @@
+from . import base
+
+class Tenant(base.Resource):
+    service = Keystone
+
+    def __init__(self, uuid, name):
+        super(Tenant, self).__init__(uuid, name)
+        self.description = ''
+        self.enabled = True
+
+    def __repr__(self):
+        return("<Tenant(uuid={0}, name={1}, description={2}, enabled={3})>"
+               .format(self.uuid, self.name, self.description, self.enabled))
+
+    def discover(self):
+        keystone = self.service.client
+        tt = keystone.tenant.get(self.uuid)
+        self.description = tt.description
+        self.enabled = tt.enabled
+
+    def migrate(self):
+        keystone = self.service.client
+        return keystone.tenants.create(self.name, 
+                                       self.description,
+                                       self.enabled)
