340caa4ef986783aee6d51f0595bc49062d31921
Oleg Gelbukh <ogelbukh@mirantis.com>
2014-06-23 14:19:17 +0000

    Add script to migrate host from source to target cloud
    Add script scripts/migrate_host.py that reads configuration file
    for the hardware environment and uses IPMI protocol to decomission
    the host specified as an argument in the command line.

---
+++ scripts/migrate_host.py
@@ -0,0 +1,177 @@
+import argparse
+import logging
+import os
+import pyipmi
+import pyipmi.bmc
+import time
+import urllib2
+import yaml
+
+from operator import attrgetter
+
+
+LOG = logging.getLogger(__name__)
+
+SOURCE_CLOUD_TAG = 'source'
+FUEL_MASTER_NODE_TAG = 'fuel.master'
+FUEL_API_IFACE_TAG = 'fuel.api'
+DEFAULT_INVENTORY_FILE='inventory.yaml'
+
+
+class Error(Exception):
+    pass
+
+
+class NotFound(Error):
+    pass
+
+
+class TimeoutException(Error):
+    pass
+
+
+def safe_load_yaml(filename):
+    with open(filename) as f:
+        return yaml.safe_load(f.read())
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Migrates physical servers from "
+                                                 "OpenStack cloud to Mirantis "
+                                                 "OpenStack cloud.")
+    parser.add_argument("-i", "--inventory",
+                        default=None,
+                        type=safe_load_yaml,
+                        help="A filename of an inventory of datacenter "
+                             "hardware")
+    parser.add_argument("-e", "--env-id",
+                        default=1,
+                        help="An ID of target Mirantis OpenStack cloud in Fuel")
+    parser.add_argument("hostname",
+                        default=None,
+                        help="A hypervisor hostname of server to migrate")
+    return parser
+
+
+def read_configuration(stream):
+    with stream as f:
+        return yaml.safe_load(f.read())
+
+
+def get_bmc(ipmi):
+    host = ipmi['host']
+    username = ipmi['auth']['user']
+    password = ipmi['auth']['password']
+    bmc = pyipmi.make_bmc(pyipmi.bmc.LanBMC,
+                          hostname=host,
+                          username=username,
+                          password=password)
+    return bmc
+
+
+def force_pxeboot(host):
+    host = get_bmc(host['ipmi'])
+    try:
+        host.set_bootdev('pxe')
+    except Exception as exc:
+        LOG.exception("Cannot set boot device to PXE: %s", exc.message)
+    try:
+        host.set_chassis_power('reset')
+    except Exception as exc:
+        LOG.exception("Cannot reset host %s: %s",
+                      host['ipmi']['host'],
+                      exc.message)
+
+
+def get_fuel_endpoint(config):
+    for host_ref in config['hosts']:
+        host = config['hosts'][host_ref]
+        if FUEL_MASTER_NODE_TAG in host['notes']:
+            fuel_master = host
+    for iface_ref in fuel_master['interfaces']:
+        iface = fuel_master['interfaces'][iface_ref]
+        if FUEL_API_IFACE_TAG in iface['tags']:
+            endpoint = iface['ip'][0]
+    return endpoint
+
+
+class Fuel(object):
+    def __init__(self, endpoint, env_id):
+        os.environ['LISTEN_ADDRESS'] = endpoint
+        from fuelclient.objects.environment import Environment
+        self.env = Environment(env_id)
+
+    def assign_role(self, node, role='compute'):
+
+        '''Deploy compute node to Mirantis OpenStack cluster via Fuel API
+
+        This function adds a host to Mirantis OpenStack cluster as a Compute node.
+        It finds node defined by its ID in Fuel API, verifies that it is in
+        'discover' status and assigns 'compute' role to it.
+
+        :param node:        Fuel Node object to assign role to
+        :param role:        role to assign to the node, defaults to 'compute'
+        '''
+        node.update()
+        if node.data['status'] == 'discover':
+            self.env.assign([node], [role])
+        elif node.data['status'] == 'error':
+            LOG.err("Node in 'error' status: %s", node.data)
+        else:
+            LOG.warn("Node already added: %s", node.data)
+
+    def get_next_id(self):
+        next_id = sorted(self.env.get_all_nodes(), key=attrgetter('id'))[-1].id + 1
+        return next_id
+
+    def wait_for_node(self, status, node_id, timeout=300):
+        from fuelclient.objects.node import Node
+        node = Node(node_id)
+        start_time = time.clock()
+        while time.clock() < (start_time + timeout):
+            try:
+                node.update()
+            except urllib2.HTTPError as exc:
+                if exc.code == 404:
+                    pass
+                else:
+                    LOG.exception("Exception while waiting for node: %s",
+                                  exc.message)
+                    raise exc
+            else:
+                break
+        else:
+            LOG.err("Timed out while waiting for node: %s",
+                    node_id)
+            return False
+        return node
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    env_id = args.env_id
+    hostname = args.hostname
+    if args.inventory is not None:
+        inventory = args.inventory
+    else:
+        inventory = safe_load_yaml(DEFAULT_INVENTORY_FILE)
+    fuel_endpoint = get_fuel_endpoint(inventory)
+    inventory_host = inventory['hosts'][hostname]
+
+    if SOURCE_CLOUD_TAG not in inventory_host['notes']:
+        fuel = Fuel(fuel_endpoint, env_id)
+        node_id = fuel.get_next_id()
+        force_pxeboot(inventory_host)
+        node = fuel.wait_for_node('discover', node_id)
+        if node:
+            fuel.assign_role(node)
+    else:
+        LOG.error("Host is not in source cloud: %s", hostname)
+
+
+if __name__ == "__main__":
+    main()
