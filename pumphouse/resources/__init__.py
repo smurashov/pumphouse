759c7ab05f96f4ad81dcfa3fe2b12149f8ff370b
Oleg Gelbukh <ogelbukh@mirantis.com>
2014-07-07 12:36:17 +0000

    Remove unused framework components

--- pumphouse/resources/__init__.py
+++
@@ -1,4 +0,0 @@
-from pumphouse.resources.flavor import Flavor
-from pumphouse.resources.tenant import Tenant
-from pumphouse.resources.role import Role
-from pumphouse.resources.image import Image
--- pumphouse/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from pumphouse.services.base import Service
-from pumphouse.services.nova import Nova
-from pumphouse.services.keystone import Keystone
-from pumphouse.services.glance import Glance
