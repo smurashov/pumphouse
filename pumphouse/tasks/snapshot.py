3cbf2730c0605dc990e31146b0a899cba321b8b6
Ilya Kharin <akscram@gmail.com>
2014-08-15 23:11:05 +0400

    Add copirates in tasks and flows

--- pumphouse/tasks/snapshot.py
+++ pumphouse/tasks/snapshot.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2014 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and#
+# limitations under the License.
+
 import logging
 
 from taskflow.patterns import linear_flow
@@ -13,7 +27,7 @@ LOG = logging.getLogger(__name__)
 class RetrieveImage(tasks.BaseCloudTask):
     def retrieve(self, image_id):
         image = self.cloud.glance.images.get(image_id)
-        return image.to_dict()            
+        return image.to_dict()
 
 
 class EnsureSnapshot(tasks.BaseCloudTask):
