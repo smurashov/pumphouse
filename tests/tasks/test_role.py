644c873fe25d9eb524ad675a56d7b20f7e3be7ea
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2014-08-20 13:49:08 +0400

    Fixes pep8 issues

--- tests/tasks/test_role.py
+++ tests/tasks/test_role.py
@@ -5,6 +5,7 @@ from pumphouse.tasks import role
 from pumphouse.exceptions import keystone_excs
 from taskflow.patterns import linear_flow
 
+
 class TestRoleCase(unittest.TestCase):
     def setUp(self):
         self.dummy_id = '123'
@@ -22,19 +23,21 @@ class TestRoleCase(unittest.TestCase):
         self.cloud.keystone.roles.find.return_value = self.role
         self.cloud.keystone.roles.create.return_value = self.role
 
+
 class TestRetrieveRole(TestRoleCase):
     def test_retrieve(self):
         role.RetrieveRole(self.cloud).retrieve(self.dummy_id)
-        
+
         # Assures that cloud.keystone.roles.get is called with the same id
         # that was passed to retrieve method
         self.cloud.keystone.roles.get.assert_called_with(self.dummy_id)
+
 
 class TestEnsureRole(TestRoleCase):
     def test_execute(self):
         ensure_role = role.EnsureRole(self.cloud)
 
-        # Assures that no cloud.keystone.roles.create method is not called 
+        # Assures that no cloud.keystone.roles.create method is not called
         # if cloud.keystone.roles.find does not raise Not Found exception
         # i.e. role is found by its name
         ensure_role.execute(self.role_info)
@@ -43,19 +46,24 @@ class TestEnsureRole(TestRoleCase):
     def test_execute_not_found(self):
         ensure_role = role.EnsureRole(self.cloud)
 
-        # In case if Not Found exception is raised by ...find call 
+        # In case if Not Found exception is raised by ...find call
         # assures that cloud.keystone.roles.create is called
         self.cloud.keystone.roles.find.side_effect = keystone_excs.NotFound
         ensure_role.execute(self.role_info)
         self.cloud.keystone.roles.create.assert_called_once_with(name="dummy")
+
 
 class TestMigrateRole(TestRoleCase):
     @patch.object(linear_flow.Flow, 'add')
     def test_migrate_role(self, mock_flow):
         mock_flow.return_value = self.dummy_id
         store = {}
-        
-        (flow, store) = role.migrate_role(self.role, self.dst, store, self.dummy_id)
+
+        (flow, store) = role.migrate_role(
+            self.role,
+            self.dst,
+            store,
+            self.dummy_id)
         # Assures linear_flow.Flow().add is called
         self.assertTrue(mock_flow.called)
 
--- tests/tasks/test_tenant.py
+++ tests/tasks/test_tenant.py
@@ -5,6 +5,7 @@ from pumphouse.tasks import tenant
 from pumphouse.exceptions import keystone_excs
 from taskflow.patterns import linear_flow
 
+
 class TenantTestCase(unittest.TestCase):
     def setUp(self):
         self.dummy_id = '123'
@@ -28,14 +29,16 @@ class TenantTestCase(unittest.TestCase):
 class TestRetrieveTenant(TenantTestCase):
     def test_retrieve(self):
         tenant.RetrieveTenant(self.cloud).retrieve(self.dummy_id)
-        # Assures cloud.keystone.tenants.get method is called with the parameter supplied
+        # Assures cloud.keystone.tenants.get method is called with
+        # the parameter supplied
         self.cloud.keystone.tenants.get.assert_called_with(self.dummy_id)
+
 
 class TestEnsureTenant(TenantTestCase):
     def test_execute(self):
         ensure_tenant = tenant.EnsureTenant(self.cloud)
 
-        # Assures that no cloud.keystone.tenants.create method is not called 
+        # Assures that no cloud.keystone.tenants.create method is not called
         # if cloud.keystone.tenants.find does not raise Not Found exception
         # i.e. tenant is found by its name
         ensure_tenant.execute(self.tenant_info)
@@ -44,25 +47,32 @@ class TestEnsureTenant(TenantTestCase):
     def test_execute_not_found(self):
         ensure_tenant = tenant.EnsureTenant(self.cloud)
 
-        # In case if Not Found exception is raised by ...find call 
+        # In case if Not Found exception is raised by ...find call
         # assures that cloud.keystone.tenants.create is called
         self.cloud.keystone.tenants.find.side_effect = keystone_excs.NotFound
         ensure_tenant.execute(self.tenant_info)
-        self.cloud.keystone.tenants.create.assert_called_once_with("dummy", description="dummydummy", enabled=True)
+        self.cloud.keystone.tenants.create.assert_called_once_with(
+            "dummy", description="dummydummy", enabled=True)
+
 
 class TestMigrateTenant(TenantTestCase):
-    
+
     @patch.object(linear_flow.Flow, 'add')
     def test_migrate_tenant(self, mock_flow):
         mock_flow.return_value = self.dummy_id
         store = {}
-        
-        (flow, store) = tenant.migrate_tenant(self.tenant, self.dst, store, self.dummy_id)
+
+        (flow, store) = tenant.migrate_tenant(
+            self.tenant,
+            self.dst,
+            store,
+            self.dummy_id)
         # Assures linear_flow.Flow().add is called
         self.assertTrue(mock_flow.called)
 
         # Assures that new flow is added to store after execution
         self.assertNotEqual({}, store)
+
 
 if __name__ == '__main__':
     unittest.main()
