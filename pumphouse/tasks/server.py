824f0fe9878c46b03f645b56db406c4f350c6b09
Ilya Kharin <akscram@gmail.com>
2014-08-27 19:37:55 +0400

    Add emiting events in migration of servers

--- pumphouse/tasks/server.py
+++ pumphouse/tasks/server.py
@@ -16,6 +16,7 @@ import logging
 
 from taskflow.patterns import linear_flow
 
+from pumphouse import events
 from pumphouse import task
 from pumphouse import flows
 from pumphouse.tasks import utils as task_utils
@@ -25,6 +26,32 @@ from pumphouse import utils
 LOG = logging.getLogger(__name__)
 
 provision_server = flows.register("provision_server")
+
+
+class ServerStartMigrationEvent(task.BaseCloudTask):
+    def execute(self, server_id):
+        LOG.info("Migration of server %r started", server_id)
+        events.emit("server migrate", {
+            "id": server_id,
+        }, namespace="/events")
+
+    # TODO(akscram): Here we can emit the event to report about
+    #                failures during migration process. It's commented
+    #                because didn't supported by UI and untested.
+#    def revert(self, server_id, result, flow_failures):
+#        LOG.info("Migration of server %r failed by reason %s",
+#                 server_id, result)
+#        events.emit("server migration failed", {
+#            "id": server_id,
+#        }, namespace="/events")
+
+
+class ServerSuccessMigrationEvent(task.BaseCloudsTask):
+    def execute(self, src_server_info, dst_server_info):
+        events.emit("server migrated", {
+            "source_id": src_server_info["id"],
+            "destination_id": dst_server_info["id"],
+        }, namespace="/events")
 
 
 class RetrieveServer(task.BaseCloudTask):
@@ -38,15 +65,29 @@ class SuspendServer(task.BaseCloudTask):
         self.cloud.nova.servers.suspend(server_info["id"])
         server = utils.wait_for(server_info["id"], self.cloud.nova.servers.get,
                                 value="SUSPENDED")
-        LOG.info("Server suspended: %s", server_info["id"])
+        self.suspend_event(server)
         return server.to_dict()
+
+    def suspend_event(self, server):
+        LOG.info("Server suspended: %s", server.id)
+        events.emit("server suspended", {
+            "id": server.id,
+            "cloud": self.cloud.name,
+        }, namespace="/events")
 
     def revert(self, server_info, result, flow_failures):
         self.cloud.nova.servers.resume(server_info["id"])
         server = utils.wait_for(server_info["id"], self.cloud.nova.servers.get,
                                 value="ACTIVE")
-        LOG.info("Server resumed: %s", server_info["id"])
+        self.resume_event(server)
         return server.to_dict()
+
+    def resume_event(self, server):
+        LOG.info("Server resumed: %s", server.id)
+        events.emit("server resumed", {
+            "id": server.id,
+            "cloud": self.cloud.name,
+        }, namespace="/events")
 
 
 class BootServerFromImage(task.BaseCloudTask):
@@ -57,14 +98,41 @@ class BootServerFromImage(task.BaseCloudTask):
                                                 flavor_info["id"])
         server = utils.wait_for(server, self.cloud.nova.servers.get,
                                 value="ACTIVE")
-        LOG.info("Server spawned: %s", server.id)
+        self.spawn_event(server)
         return server.to_dict()
+
+    def spawn_event(self, server):
+        LOG.info("Server spawned: %s", server.id)
+        try:
+            hostname = getattr(server, "OS-EXT-SRV-ATTR:hypervisor_hostname")
+        except AttributeError as err:
+            LOG.warning("Could not get 'hypervisor_hostname' attribute from "
+                        "server %r: %s", server.id, err)
+        else:
+            events.emit("server boot", {
+                "cloud": self.cloud.name,
+                "id": server.id,
+                "name": server.name,
+                "tenant_id": server.tenant_id,
+                # XXX(akscram): It may suitable only for images
+                #               (untested for snapshots)
+                "image_id": server.image["id"],
+                "host_name": hostname,
+                "status": "active",
+            }, namespace="/events")
 
 
 class TerminateServer(task.BaseCloudTask):
     def execute(self, server_info):
         self.cloud.nova.servers.delete(server_info["id"])
-        LOG.info("Server terminated: %s", server_info["id"])
+        self.terminate_event(server_info)
+
+    def terminate_event(self, server):
+        LOG.info("Server terminated: %s", server["id"])
+        events.emit("server terminate", {
+            "cloud": self.cloud.name,
+            "id": server["id"],
+        }, namespace="/events")
 
 
 @provision_server.add("image")
@@ -103,6 +171,8 @@ def reprovision_server(src, dst, store, server_id, image_id, flavor_id):
 
 @provision_server.add("snapshot")
 def reprovision_server_with_snapshot(src, dst, store, server_id, flavor_id):
+    server_start_event = "server-{}-start-event".format(server_id)
+    server_finish_event = "server-{}-finish-event".format(server_id)
     server_sync = "server-{}-sync".format(server_id)
     server_binding = "server-{}".format(server_id)
     server_retrieve = "server-{}-retrieve".format(server_id)
@@ -114,6 +184,9 @@ def reprovision_server_with_snapshot(src, dst, store, server_id, flavor_id):
     flow = linear_flow.Flow("migrate-server-{}".format(server_id))
     flow.add(task_utils.SyncPoint(name=server_sync,
                                   requires=[image_ensure, flavor_ensure]))
+    flow.add(ServerStartMigrationEvent(src,
+                                       name=server_start_event,
+                                       rebind=[server_binding]))
     flow.add(RetrieveServer(src,
                             name=server_binding,
                             provides=server_retrieve,
@@ -131,5 +204,9 @@ def reprovision_server_with_snapshot(src, dst, store, server_id, flavor_id):
     flow.add(TerminateServer(src,
                              name=server_terminate,
                              rebind=[server_suspend]))
+    flow.add(ServerSuccessMigrationEvent(src, dst,
+                                         name=server_finish_event,
+                                         rebind=[server_retrieve,
+                                                 server_boot]))
     store[server_binding] = server_id
     return (flow, store)
