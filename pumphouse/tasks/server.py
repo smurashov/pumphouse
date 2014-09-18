169e0b653c704c10d90dd46d5249b0949f2b7803
Oleg Gelbukh <ogelbukh@mirantis.com>
2014-09-18 07:22:28 +0000

    Remove checks trigger from server migration flow

--- pumphouse/tasks/server.py
+++ pumphouse/tasks/server.py
@@ -181,9 +181,6 @@ def reprovision_server(src, dst, store, server, image_ensure):
     floating_ips_flow, store = restore_floating_ips(src, dst, store,
                                                     server.to_dict())
     flow.add(floating_ips_flow)
-    dst_check_task, store = check_tasks.run_checks(src, dst, store,
-                                                   server_id, None)
-    flow.add(dst_check_task)
     flow.add(TerminateServer(src,
                              name=server_terminate,
                              rebind=[server_suspend]))
