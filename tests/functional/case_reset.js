b6f07cbb28861e80c57f1424c7482d6facfb57de
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2014-10-09 16:56:47 +0400

    Introduces that=this optimization via bind(this)

--- tests/functional/case_reset.js
+++ tests/functional/case_reset.js
@@ -3,23 +3,22 @@ var TestCase = require('./test_case');
 var ResetTestCase = new TestCase('Environments reset');
 
 ResetTestCase.addStep('Calling API to reset', function() {
-    var that = this;
     this.test_case.api.reset(function(err, res) {
-        if (err) that.fail('Resetting error');
-        that.next();
-    })
+        if (err) this.fail('Resetting error');
+        this.next();
+    }.bind(this));
 });
 
 ResetTestCase.addStep('Fetching reset completion events for both clouds', function() {
-    var completed_for = {}, that = this;
+    var completed_for = {};
     this.test_case.events.on('reset completed', function(m) {
         completed_for[m.cloud] = true;
         if (completed_for['source'] && completed_for['destination']) {
             console.log('Reset completed');
-            return that.next();
+            return this.next();
         }
         return false;
-    })
+    }.bind(this))
 });
 
 exports.o = ResetTestCase;
