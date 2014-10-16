5d6499a5c94c1a1a4680d2e0bfba888d2d80b03f
Ryabin Sergey <sryabin@mirantis.com>
2014-10-16 15:09:40 +0000

    Fixed multiple typos

--- pumphouse/tasks/network/neutron/network.py
+++ pumphouse/tasks/network/neutron/network.py
@@ -3,12 +3,116 @@ import logging
 from taskflow.patterns import graph_flow
 
 from pumphouse import task
-#from pumphouse import events
-from pumphouse import exceptions
-from pumphouse.tasks import utils as task_utils
 
 LOG = logging.getLogger(__name__)
-#TODO (sryabin) create|del|list_security_group_rule implementation
+# TODO (sryabin) create|del|list_security_group_rule implementation
+
+
+def list_ports(client):
+    # list_ports(fields=['network_id', 'mac_address', 'id'])
+    try:
+        return {
+            'port_list': lambda: client.list_ports()["ports"]
+        }
+    except Exception as e:
+        LOG.exception("Error in listing ports: %s" % e.message)
+        raise
+
+
+def del_port(client, port_id):
+    try:
+        return client.delete_port(port_id)
+    except Exception as e:
+        LOG.exception("Error in delete port: %s, pord_id: %s" %
+                      (e.message, port_id))
+        raise
+
+
+def create_port(client, network_id, mac, security_groups, ip_address):
+    port_params = {
+        'network_id': network_id,
+        'mac_address': mac,
+        'security_groups': security_groups
+    }
+    if (ip_address):
+        port_params['fixed_ips'] = [{"ip_address": ip_address}]
+
+    # TODO Keyerror
+    try:
+        return client.create_port({'port': port_params})['port']
+    except Exception as e:
+        LOG.exception("Error in create_port: %s, network_id: %s, mac: %s, \
+            ip_address: %s" % (e.message, network_id, mac, ip_address))
+        raise
+
+
+def get_security_groups(client):
+    try:
+        return client.list_security_groups()['security_groups']
+    except Exception as e:
+        LOG.exception("Error in get security groups: %s" % e.message)
+        raise
+
+
+def del_security_groups(client, security_group):
+    try:
+        client.delete_security_group(security_group)
+    except Exception as e:
+        LOG.exception("Error in get security groups: %s" % e.message)
+        raise
+
+
+def create_security_group(client, name, description):
+    try:
+        return client.create_sequrity_group({
+            "security_group": {
+                "name": name,
+                "description": description,
+            },
+        })
+    except Exception as e:
+        LOG.exception("Error in get security groups: %s" % e.message)
+        raise
+
+
+def list_subnets(client):
+    try:
+        return client.list_subnets()['subnets']
+    except Exception as e:
+        LOG.exception("Error in list subnets: %s" % e.message)
+        raise
+
+
+def del_subnet(client, subnet_id):
+    try:
+        return client.list_subnets()['subnets']
+    except Exception as e:
+        LOG.exception("Error in list subnets: %s" % e.message)
+        raise
+
+
+def create_subnet(client, network_name):
+    try:
+        return client.create_network({
+                                     'network': network_name
+                                     })
+    except Exception as e:
+        LOG.exception("Error in list subnets: %s" % e.message)
+        raise
+
+
+def list_network(client, net_info, tenant_id):
+    try:
+        if net_info:
+            if 'id' in net_info:
+                return client.list_networks(id=net_info['id'])['networks'][0]
+            elif 'name' in net_info:
+                return client.list_networks(name=net_info['name'])['networks'][0]
+        else:
+            return client.list_networks()
+    except Exception as e:
+        LOG.exception("Error in get network: %s" % e.message)
+        raise
 
 
 class RetrieveAllNetworks(task.BaseCloudTask):
@@ -70,110 +174,3 @@ def migrate_network(context, network_id=None):
                            rebind=[all_dst_networks, network_retrieve]))
 
     return flow, network_ensure
-
-
-def list_ports(client):
-    # list_ports(fields=['network_id', 'mac_address', 'id'])
-    try:
-        return {
-            'port_list': lambda: client.list_ports()["ports"]
-        }
-    except Exception as e:
-        LOG.exception("Error in listing ports: %s" % e.message)
-        raise
-
-
-def del_port(client, port_id):
-    try:
-        return client.delete_port(port_id)
-    except Exception as e:
-        LOG.exception("Error in delete port: %s, pord_id: %s" %
-                      (e.message, port_id))
-        raise
-
-
-def create_port(client, network_id, mac, security_groups, ip_address):
-    port_params = {
-        'network_id': network_id,
-        'mac_address': mac,
-        'security_groups': security_groups
-    }
-    if (ip_address):
-        port_params['fixed_ips'] = [{"ip_address": ip_address}]
-
-    # TODO Keyerror
-    try:
-        return client.create_port({'port': port_params})['port']
-    except Exception as e:
-        LOG.exception("Error in create_port: %s, network_id: %s, mac: %s, ip_address: %s" %
-                      (e.message, network_id, mac, ip_address))
-        raise
-
-
-def get_security_groups(client):
-    try:
-        return client.list_security_groups()['security_groups']
-    except Exception as e:
-        LOG.exception("Error in get security groups: %s" % e.message)
-        raise
-
-
-def del_security_groups(client, security_group):
-    try:
-        client.delete_security_group(security_group)
-    except Exception as e:
-        LOG.exception("Error in get security groups: %s" % e.message)
-        raise
-
-
-def create_security_group(client, name, description):
-    try:
-        return client.create_sequrity_group({
-            "security_group": {
-                "name": name,
-                "description": description,
-            },
-        })
-    except Exception as e:
-        LOG.exception("Error in get security groups: %s" % e.message)
-        raise
-
-
-def list_subnets(client):
-    try:
-        return client.list_subnets()['subnets']
-    except Exception as e:
-        LOG.exception("Error in list subnets: %s" % e.message)
-        raise
-
-
-def del_subnet(client, subnet_id):
-    try:
-        return client.list_subnets()['subnets']
-    except Exception as e:
-        LOG.exception("Error in list subnets: %s" % e.message)
-        raise
-
-
-def create_subnet(client, network_name):
-    try:
-        return client.create_network({
-                                     'network': network_name
-                                     })
-    except Exception as e:
-        LOG.exception("Error in list subnets: %s" % e.message)
-        raise
-
-
-def list_network(client, network_info, tenant_id):
-    try:
-        if network_info:
-            if 'id' in network_info:
-                return client.list_networks(id=network_info['id'])['networks'][0]
-            elif 'name' in network_info:
-                return client.list_networks(name=network_info['name'])['networks'][0]
-        else:
-            return client.list_networks()
-    except Exception as e:
-        LOG.exception("Error in get network: %s" % e.message)
-        raise
