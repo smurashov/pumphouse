089b6bbb4225b1827ec378bc7a82104dd4fed1f9
Oleg Gelbukh <ogelbukh@mirantis.com>
2014-12-22 11:59:46 +0000

    Single flow for project resources migration
    Use single flow for project migration instead of creating flow of it's own for
    every resource type.

--- pumphouse/tasks/project_resources.py
+++ pumphouse/tasks/project_resources.py
@@ -14,7 +14,7 @@
 
 import logging
 
-from taskflow.patterns import graph_flow, unordered_flow
+from taskflow.patterns import graph_flow
 
 from pumphouse.tasks import server_resources
 from pumphouse.tasks import image as image_tasks
@@ -25,38 +25,31 @@ from pumphouse.tasks import identity as identity_tasks
 LOG = logging.getLogger(__name__)
 
 
-def migrate_project_servers(context, tenant_id):
+def migrate_project_servers(context, flow, tenant_id):
     servers = context.src_cloud.nova.servers.list(
         search_opts={'all_tenants': 1, 'tenant_id': tenant_id})
-    flow = graph_flow.Flow("migrate-resources-{}".format(tenant_id))
-    servers_flow = unordered_flow.Flow("migrate-servers-{}".format(tenant_id))
     migrate_server = server_resources.migrate_server
     for server in servers:
         server_binding = "server-{}".format(server.id)
         if server_binding not in context.store:
             resources, server_flow = migrate_server(context, server.id)
             flow.add(*resources)
-            servers_flow.add(server_flow)
-    flow.add(servers_flow)
-    return flow
+            flow.add(server_flow)
 
 
-def migrate_project_images(context, tenant_id):
+def migrate_project_images(context, flow, tenant_id):
     images = context.src_cloud.glance.images.list(
         filters={"owner": tenant_id})
-    flow = unordered_flow.Flow("migrate-project-{}-images".format(tenant_id))
     for image in list(images):
         image_binding = "image-{}".format(image.id)
         if image_binding not in context.store:
             image_flow = image_tasks.migrate_image(context, image.id)
             flow.add(image_flow)
-    return flow
 
 
-def migrate_project_volumes(context, tenant_id):
+def migrate_project_volumes(context, flow, tenant_id):
     volumes = context.src_cloud.cinder.volumes.list(
         search_opts={"all_tenants": 1})
-    flow = unordered_flow.Flow("migrate-project-{}-volumes".format(tenant_id))
     for volume in volumes:
         volume_tenant_id = getattr(volume, "os-vol-tenant-attr:tenant_id")
         volume_binding = "volume-{}".format(volume.id)
@@ -66,17 +59,13 @@ def migrate_project_volumes(context, tenant_id):
             volume_flow = volume_tasks.migrate_detached_volume(
                 context, volume.id, None, tenant_id)
             flow.add(volume_flow)
-    return flow
 
 
 def migrate_project(context, project_id):
     flow = graph_flow.Flow("migrate-project-{}".format(project_id))
-    server_flow = migrate_project_servers(context, project_id)
-    images_flow = migrate_project_images(context, project_id)
+    migrate_project_servers(context, flow, project_id)
+    migrate_project_images(context, flow, project_id)
+    migrate_project_volumes(context, flow, project_id)
     _, identity_flow = identity_tasks.migrate_identity(context, project_id)
-    volumes_flow = migrate_project_volumes(context, project_id)
-    flow.add(server_flow,
-             images_flow,
-             identity_flow,
-             volumes_flow)
+    flow.add(identity_flow)
     return flow
