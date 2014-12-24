96963620cfdb58555d9bf103c4c86166cd103c02
Ryabin Sergey <sryabin@mirantis.com>
2014-12-24 12:43:26 +0000

    Split neutron flow into separate files

---
+++ pumphouse/tasks/network/neutron/securitygroup.py
@@ -0,0 +1,156 @@
+# Copyright (c) 2014 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and#
+# limitations under the License.
+
+import logging
+
+from pumphouse import task
+from taskflow.patterns import graph_flow
+
+LOG = logging.getLogger(__name__)
+
+
+def get_securityGroups_by(neutron, security_groups_filter):
+    try:
+        return neutron.list_security_groups(
+            **security_groups_filter)['security_groups']
+
+    except Exception as e:
+        raise e
+
+
+def create_securityGroup_rule(neutron, security_group_rule_info):
+    try:
+        return neutron.create_security_group_rule(
+            {'security_group_rule': security_group_rule_info})
+    except Exception as e:
+        raise e
+
+
+def create_security_group(neutron, security_group_info):
+    try:
+        return neutron.create_security_group(
+            {'security_group': security_group_info})['security_group']
+    except Exception as e:
+        raise e
+
+
+class RetrieveSecurityGroups(task.BaseCloudTask):
+
+    def execute(self):
+        return get_securityGroups_by(self.cloud.neutron, {})
+
+
+class RetrieveSecurityGroupById(task.BaseCloudTask):
+
+    def execute(self, all_security_groups, security_id):
+        for security_group in all_security_groups:
+            if (security_group['id'] == security_id):
+                return security_group
+
+        return None
+
+
+class EnsureSecurityGroup(task.BaseCloudTask):
+
+    def execute(self, all_security_groups, security_group_info, port_info):
+
+        for security_group in all_security_groups:
+            if (security_group['name'] == security_group_info['name']):
+                # XXX check security group assigned to port
+                LOG.info("security '%s' group already exists %s" %
+                         (security_group['name'], str(security_group)))
+                return security_group
+
+        LOG.info("security '%s' group not exists" %
+                 security_group_info['name'])
+
+        security_group_rules = security_group_info['security_group_rules']
+
+        del security_group_info['id'], security_group_info[
+            'security_group_rules']
+        security_group = create_security_group(
+            self.cloud.neutron, security_group_info)
+
+        for rule in security_group_rules:
+            del rule['id'], rule['tenant_id']
+            rule['security_group_id'] = security_group['id']
+            create_securityGroup_rule(self.cloud.neutron, rule)
+
+        if security_group['id'] not in port_info['security_groups']:
+            port_info['security_groups'].append(security_group['id'])
+            self.cloud.neutron.update_port(port_info['id'], {
+                'port': {'security_groups': port_info['security_groups']}
+            })
+
+        return security_group
+
+
+def migrate_securityGroup(context, securityGroup_id, port_binding):
+    securityGroup_binding = securityGroup_id
+
+    securityGroup_retrieve = "\
+        securityGroup-{}-retrieve".format(securityGroup_binding)
+    securityGroup_ensure = "\
+        securityGroup-{}-ensure".format(
+        securityGroup_binding)
+
+    if (securityGroup_binding in context.store):
+        return None, securityGroup_ensure
+
+    context.store[securityGroup_binding] = securityGroup_id
+
+    f = graph_flow.Flow(
+        "neutron-securityGroup-migration-{}".format(securityGroup_id))
+
+    all_src_securityGroup_binding = "srcNeutronAllSecurityGroups"
+    all_dst_securityGroup_binding = "dstNeutronAllSecurityGroups"
+
+    if (all_src_securityGroup_binding not in context.store):
+
+        f.add(RetrieveSecurityGroups(
+            context.src_cloud,
+            name="retrieveAllSrcSecurityGroups",
+            provides=all_src_securityGroup_binding
+        ))
+
+        context.store[all_src_securityGroup_binding] = None
+
+    if (all_dst_securityGroup_binding not in context.store):
+
+        f.add(RetrieveSecurityGroups(
+            context.dst_cloud,
+            name="retrieveAllDstSecurityGroups",
+            provides=all_dst_securityGroup_binding
+        ))
+
+        context.store[all_dst_securityGroup_binding] = None
+
+    f.add(RetrieveSecurityGroupById(context.src_cloud,
+                                    name=securityGroup_retrieve,
+                                    provides=securityGroup_retrieve,
+                                    rebind=[
+                                        all_src_securityGroup_binding,
+                                        securityGroup_binding
+                                    ]))
+
+    f.add(EnsureSecurityGroup(context.dst_cloud,
+                              name=securityGroup_ensure,
+                              provides=securityGroup_ensure,
+                              rebind=[
+                                  all_dst_securityGroup_binding,
+                                  securityGroup_retrieve,
+                                  port_binding
+                              ]))
+
+    return f, securityGroup_ensure
---
+++ pumphouse/tasks/network/neutron/subnet.py
@@ -0,0 +1,136 @@
+# Copyright (c) 2014 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and#
+# limitations under the License.
+
+import logging
+
+from pumphouse import task
+from taskflow.patterns import graph_flow
+
+
+LOG = logging.getLogger(__name__)
+
+
+def get_subnet_by(neutron, subnet_filter):
+    try:
+        return neutron.list_subnets(**subnet_filter)['subnets']
+    except Exception as e:
+        raise e
+
+
+def create_subnet(neutron, subnet_info):
+    try:
+        return neutron.create_subnet({'subnet': subnet_info})['subnet']
+    except Exception as e:
+        raise e
+
+
+class RetrieveAllSubnets(task.BaseCloudTask):
+
+    def execute(self):
+        return get_subnet_by(self.cloud.neutron, {})
+
+
+class RetrieveSubnetById(task.BaseCloudTask):
+
+    def execute(self, subnets, subnet_id):
+        for subnet in subnets:
+            if (subnet['id'] == subnet_id):
+                return subnet
+
+        return None
+
+
+class EnsureSubnet(task.BaseCloudTask):
+
+    def execute(self, subnets, subnet_info, network_info, tenant_info):
+        for subnet in subnets:
+            if (subnet['name'] == subnet_info['name']):
+                LOG.info("Subnet %s is allready exists, name: %s" %
+                         (subnet_info['id'], subnet_info['name']))
+                return subnet
+
+        LOG.info("Subnet not found, name: %s" % subnet_info['name'])
+
+        subnet = create_subnet(self.cloud.neutron, {
+            'allocation_pools': subnet_info['allocation_pools'],
+            'cidr': subnet_info['cidr'],
+            'ip_version': subnet_info['ip_version'],
+            'gateway_ip': subnet_info['gateway_ip'],
+            'name': subnet_info['name'],
+            'network_id': network_info['id'],
+            'tenant_id': tenant_info['id']
+        })
+
+        LOG.info("Subnet %s created: %s" % (subnet['id'], str(subnet)))
+
+        return subnet
+
+
+def migrate_subnet(context, subnet_id, network_info, tenant_info):
+
+    subnet_binding = subnet_id
+
+    subnet_retrieve = "subnet-{}-retrieve".format(
+        subnet_binding)
+    subnet_ensure = "subnet-{}-ensure".format(subnet_binding)
+
+    if (subnet_binding in context.store):
+        return None, subnet_ensure
+
+    context.store[subnet_binding] = subnet_id
+
+    f = graph_flow.Flow("neutron-subnet-migration-{}".format(subnet_id))
+
+    all_src_subnet_binding = "srcNeutronAllSubnets"
+    all_dst_subnet_binding = "dstNeutronAllSubnets"
+
+    if (all_src_subnet_binding not in context.store):
+
+        f.add(RetrieveAllSubnets(
+            context.src_cloud,
+            name="retrieveAllSrcSubnets",
+            provides=all_src_subnet_binding
+        ))
+
+        context.store[all_src_subnet_binding] = None
+
+    if (all_dst_subnet_binding not in context.store):
+
+        f.add(RetrieveAllSubnets(
+            context.dst_cloud,
+            name="retrieveAllDstSubnets",
+            provides=all_dst_subnet_binding
+        ))
+
+        context.store[all_dst_subnet_binding] = None
+
+    f.add(RetrieveSubnetById(context.src_cloud,
+                             name=subnet_retrieve,
+                             provides=subnet_retrieve,
+                             rebind=[
+                                 all_src_subnet_binding,
+                                 subnet_binding
+                             ]))
+
+    f.add(EnsureSubnet(context.dst_cloud,
+                       name=subnet_ensure,
+                       provides=subnet_ensure,
+                       rebind=[
+                           all_dst_subnet_binding,
+                           subnet_retrieve,
+                           network_info,
+                           tenant_info
+                       ]))
+
+    return f, subnet_ensure
