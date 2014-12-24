775232de47fad4077b330110cff73134953acc34
Ryabin Sergey <sryabin@mirantis.com>
2014-12-24 15:14:45 +0000

    Fix migrate_nic import

--- pumphouse/tasks/network/neutron/__init__.py
+++ pumphouse/tasks/network/neutron/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and#
 # limitations under the License.
 
-from .network import migrate_nic  # noqa
+from .port import migrate_nic  # noqa
