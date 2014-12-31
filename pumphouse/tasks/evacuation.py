3a1bf6e60ab339ffb05c4cc1a4c12cb6da421c65
Yuriy Taraday <yorik.sar@gmail.com>
2014-12-31 17:37:50 +0300

    Add volume evacuation factory

--- pumphouse/tasks/evacuation.py
+++ pumphouse/tasks/evacuation.py
@@ -15,10 +15,13 @@
 import logging
 
 from taskflow.patterns import graph_flow
+from taskflow.patterns import linear_flow
+from taskflow import retry
 
 from pumphouse import exceptions
 from pumphouse.tasks import server as server_tasks
 from pumphouse.tasks import service as service_tasks
+from pumphouse.tasks import volume as volume_tasks
 
 
 LOG = logging.getLogger(__name__)
@@ -46,4 +49,77 @@ def evacuate_servers(context, hostname):
             for server in hyperv.servers:
                 server_tasks.evacuate_server(context, flow, server["uuid"],
                                              requires=[disable_services])
+    return flow
+
+
+def evacuate_volumes(context, hostname):
+    cloud = context.src_cloud
+    services = cloud.cinder.services.list(binary="cinder-volume")
+    hosts = [s.host for s in services
+             if s.host != hostname and s.status == "enabled"]
+    volumes = cloud.cinder.volumes.list(search_opts={"all_tenants": 1})
+    volumes = [v for v in volumes
+               if volume_tasks.get_volume_host(v) == hostname]
+    flow = linear_flow.Flow("evacuate-volumes-{}".format(hostname))
+    disable_cinder = "disable-cinder-volume-{}".format(hostname)
+    flow.add(service_tasks.DiableServiceWithRollback(
+        "cinder-volume",
+        cloud,
+        client="cinder",
+        name=disable_cinder,
+        provides=disable_cinder,
+        inject={"hostname": hostname},
+    ))
+    for volume in volumes:
+        try:
+            attachment = volume.attachments[0]
+        except IndexError:
+            attachment = None
+        if attachment:
+            detach_volume = "detach-volume-{}".format(volume.id)
+            volume_flow = linear_flow.Flow(detach_volume)
+            volume_flow.add(volume_tasks.DetachVolume(
+                cloud,
+                name=detach_volume,
+                provides=detach_volume,
+                inject={"attachment": attachment},
+            ))
+        evacuate_volume = "evacuate-volume-{}".format(volume.id)
+        evacuate_volume_host = "evacuate-volume-{}-host".format(volume.id)
+        evacuate_flow = linear_flow.Flow(
+            evacuate_volume,
+            retry=retry.ForEach(
+                values=hosts,
+                provides=evacuate_volume_host,
+            ),
+        )
+        volume_key = "volume-{}".format(volume.id)
+        context.store[volume_key] = volume
+        evacuate_flow.add(volume_tasks.EvacuateVolume(
+            cloud,
+            name=evacuate_volume,
+            provides=evacuate_volume,
+            rebind=[volume_key, evacuate_volume_host],
+        ))
+        if attachment:
+            volume_flow.add(evacuate_flow)
+            restore_attachment = "reattach-volume-{}".format(volume.id)
+            volume_flow.add(volume_tasks.RestoreAttachment(
+                cloud,
+                name=restore_attachment,
+                provides=restore_attachment,
+                inject={"attachment": attachment},
+            ))
+            flow.add(volume_flow)
+        else:
+            flow.add(evacuate_flow)
+    return flow
+
+
+def evacuate_host(context, hostname):
+    flow = linear_flow.Flow("evacuate-host-{}".format(hostname))
+    flow.add(
+        evacuate_servers(context, hostname),
+        evacuate_volumes(context, hostname),
+    )
     return flow
